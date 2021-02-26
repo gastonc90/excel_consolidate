d9438e13e1a82c87c06ec4b3136e4648e81fd94c
pelo <42307568+gastonc90@users.noreply.github.com>
2021-02-26 11:53:41 -0300

    Update excel_consolidate.py

--- excel_consolidate.py
+++ excel_consolidate.py
@@ -6,7 +6,7 @@ all = []
 
 
 for f in glob.glob("*.xlsx"):
-    df = pd.read_excel(f, header = 1, engine = 'openpyxl')
+    df = pd.read_excel(f, header = 0, engine = 'openpyxl')
     all.append(df)
 
 df = pd.concat(all,ignore_index='False')
@@ -14,3 +14,5 @@ df = pd.concat(all,ignore_index='False')
 print(df)
 
 df.to_excel("final_excel.xlsx")
+
+print ("Bounch of excel files consolidated in one single file (:")
